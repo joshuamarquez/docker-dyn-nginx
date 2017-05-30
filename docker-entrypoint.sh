806cbf398664f57443929ce46825fcdf9084ebcd
Joshua Marquez N <joshua.marquezn@gmail.com>
2017-05-30 11:29:12 -0700

    All .conf files removed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,19 +2,19 @@
 
 set -eo pipefail
 
-if [ "$1" == "nginx" ]; then
-  IFS=',' read -ra TPLS <<< "$TEMPLATES"
-  for i in "${TPLS[@]}"; do
-    template=${i//_/-}
-    template=${template,,}
+rm -rf /etc/nginx/conf.d/*.conf
 
-    if [ -f "/templates/${template}.template" ]; then
-      envsubst "$(</templates/${template}.list)" < /templates/${template}.template > /etc/nginx/conf.d/$template.conf
-      echo "Template ${template} applied!"
-    else
-      echo "Invalid template ${template}!"
-    fi
-  done
-fi
+IFS=',' read -ra TPLS <<< "$TEMPLATES"
+for i in "${TPLS[@]}"; do
+  template=${i//_/-}
+  template=${template,,}
+
+  if [ -f "/templates/${template}.template" ]; then
+    envsubst "$(</templates/${template}.list)" < /templates/${template}.template > /etc/nginx/conf.d/$template.conf
+    echo "Template ${template} applied!"
+  else
+    echo "Template ${template} not found!"
+  fi
+done
 
 exec "$@"
